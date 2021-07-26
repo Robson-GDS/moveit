22bb0d496141f5afa8cd2408503984d00138aad7
Robson Gomes <robson.080490@gmail.com>
2021-07-26 12:07:49 -0300

    adicionando  title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,17 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
+import Head from 'next/head';
+
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
