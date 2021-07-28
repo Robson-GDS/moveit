62f8c4db779525b4015800867a9521e5c98e2db5
Robson Gomes <robson.080490@gmail.com>
2021-07-28 12:15:39 -0300

    iniciando funcionalidade countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
+  const [time, setTime] = useState(25 * 60);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRigth] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRigth] = String(seconds).padStart(2, '0').split('');
+
   return (
     <div>
       <div className={styles.countdownContainer}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRigth}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRigth}</span>
         </div>     
       </div>
 
