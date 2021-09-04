6a45697da70234fd6c4b54f8e4b4b59455a61975
Robson Gomes <robson.080490@gmail.com>
2021-09-04 09:06:27 -0300

    add context API

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, useState, ReactNode } from 'react';
+
+export const ChallengesContext = createContext({});
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(1);
+
+  function levelUp() {
+    setLevel(level + 1)
+  }
+
+  return (
+    <ChallengesContext.Provider value={{ level, levelUp }}>
+      {children}
+    </ChallengesContext.Provider>
+  )
+}
