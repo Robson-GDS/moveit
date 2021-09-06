10fbab15176eabf6f57f1ba57146900ae153be0a
Robson Gomes <robson.080490@gmail.com>
2021-09-06 07:28:57 -0300

    criando contexto de desafios

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
   const [time, setTime] = useState(0.1 * 60);
   const [isActive, setisActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
@@ -32,6 +35,7 @@ export function Countdown() {
     } else if (isActive && time === 0){
       setHasFinished(true);
       setisActive(false);
+      startNewChallenge();
     }
   }, [isActive, time])
 
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,20 +1,42 @@
 import { createContext, useState, ReactNode } from 'react';
 
-export const ChallengesContext = createContext({});
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
 
 interface ChallengesProviderProps {
   children: ReactNode;
 }
 
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
 
   function levelUp() {
     setLevel(level + 1)
   }
 
+  function startNewChallenge() {
+    console.log('New challenge');
+  }
+
   return (
-    <ChallengesContext.Provider value={{ level, levelUp }}>
+    <ChallengesContext.Provider 
+      value={{ 
+        level, 
+        currentExperience, 
+        challengesCompleted, 
+        levelUp,
+        startNewChallenge, 
+      }}
+    >
       {children}
     </ChallengesContext.Provider>
   )
