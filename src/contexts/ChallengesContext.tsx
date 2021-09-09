326d9451ae2453efd454ce8135686ca4c38f8195
Robson Gomes <robson.080490@gmail.com>
2021-09-09 07:57:34 -0300

    incrementando ganho de xp

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -10,6 +10,7 @@ interface Challenge {
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
+  experienceToNextLevel: number;
   challengesCompleted: number;
   activeChallenge: Challenge;
   levelUp: () => void;
@@ -29,6 +30,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   function levelUp() {
     setLevel(level + 1)
@@ -50,6 +53,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
       value={{ 
         level, 
         currentExperience, 
+        experienceToNextLevel,
         challengesCompleted, 
         levelUp,
         startNewChallenge,
