1ddbf29002f79dc94899e7d6255706b2a42c9ecc
Robson Gomes <robson.080490@gmail.com>
2021-09-30 07:25:27 -0300

    adicionando audio no desafio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -48,6 +48,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     setActiveChallenge(challenge)
 
+    new Audio('/notification.mp3').play();
+
     if (Notification.permission === 'granted') {
       new Notification('Novo desafio', {
         body: `Valendo ${challenge.amount}xp!`
