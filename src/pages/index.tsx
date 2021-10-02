0d24d69f54c5ddf0ea6e582d4b00651afc09203e
Robson Gomes <robson.080490@gmail.com>
2021-10-02 10:56:57 -0300

    addGetServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
@@ -9,7 +10,9 @@ import { ChallengeBox } from "../components/ChallengeBox";
 import styles from '../styles/pages/Home.module.css'
 import { CountdownProvider } from '../contexts/CoutdownContext';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
+
   return (
     <div className={styles.container}>
       <Head>
@@ -32,4 +35,17 @@ export default function Home() {
       </CountdownProvider>
     </div>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level,
+      currentExperience,
+      challengesCompleted
+    }
+  }
 }
